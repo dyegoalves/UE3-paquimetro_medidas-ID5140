71fb148be78f67a6ddbb7ac27f9d28213b7ee84a
Dyego Alves <dyego.santos.alves@gmail.com>
2021-04-29 07:36:31 -0300

    Inicial layout e medidas alinhadas

---
+++ code0.js
@@ -0,0 +1,20 @@
+gdjs.paquimetro_95medidasCode = {};
+
+gdjs.paquimetro_95medidasCode.conditionTrue_0 = {val:false};
+gdjs.paquimetro_95medidasCode.condition0IsTrue_0 = {val:false};
+
+
+gdjs.paquimetro_95medidasCode.eventsList0 = function(runtimeScene) {
+
+};
+
+gdjs.paquimetro_95medidasCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+
+gdjs.paquimetro_95medidasCode.eventsList0(runtimeScene);
+return;
+
+}
+
+gdjs['paquimetro_95medidasCode'] = gdjs.paquimetro_95medidasCode;
