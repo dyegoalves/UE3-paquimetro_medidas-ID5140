2ae19d7eea35f9a0d87025d424272e4047480eac
Dyego Alves <dyego.santos.alves@gmail.com>
2021-05-08 19:15:53 -0300

    Fix drag mouse mobile fail tela v0.1.3

--- code0.js
+++ code0.js
@@ -19,73 +19,68 @@ gdjs.paquimetroCode.GDblockNanioPonto0Objects2= [];
 gdjs.paquimetroCode.conditionTrue_0 = {val:false};
 gdjs.paquimetroCode.condition0IsTrue_0 = {val:false};
 gdjs.paquimetroCode.condition1IsTrue_0 = {val:false};
-gdjs.paquimetroCode.condition2IsTrue_0 = {val:false};
 
 
-gdjs.paquimetroCode.mapOfGDgdjs_46paquimetroCode_46GDblockNanioPonto0Objects1Objects = Hashtable.newFrom({"blockNanioPonto0": gdjs.paquimetroCode.GDblockNanioPonto0Objects1});gdjs.paquimetroCode.mapOfGDgdjs_46paquimetroCode_46GDnanioObjects1Objects = Hashtable.newFrom({"nanio": gdjs.paquimetroCode.GDnanioObjects1});gdjs.paquimetroCode.mapOfGDgdjs_46paquimetroCode_46GDblockNanioPonto0Objects1Objects = Hashtable.newFrom({"blockNanioPonto0": gdjs.paquimetroCode.GDblockNanioPonto0Objects1});gdjs.paquimetroCode.mapOfGDgdjs_46paquimetroCode_46GDblockNanioPonto90Objects1Objects = Hashtable.newFrom({"blockNanioPonto90": gdjs.paquimetroCode.GDblockNanioPonto90Objects1});gdjs.paquimetroCode.eventsList0 = function(runtimeScene) {
-
-{
-
-
-{
-}
-
-}
-
+gdjs.paquimetroCode.eventsList0 = function(runtimeScene) {
 
 {
 
-gdjs.copyArray(runtimeScene.getObjects("blockNanioPonto0"), gdjs.paquimetroCode.GDblockNanioPonto0Objects1);
-gdjs.copyArray(runtimeScene.getObjects("nanio"), gdjs.paquimetroCode.GDnanioObjects1);
 
 gdjs.paquimetroCode.condition0IsTrue_0.val = false;
-gdjs.paquimetroCode.condition1IsTrue_0.val = false;
 {
-for(var i = 0, k = 0, l = gdjs.paquimetroCode.GDnanioObjects1.length;i<l;++i) {
-    if ( gdjs.paquimetroCode.GDnanioObjects1[i].getBehavior("Draggable").isDragged() ) {
-        gdjs.paquimetroCode.condition0IsTrue_0.val = true;
-        gdjs.paquimetroCode.GDnanioObjects1[k] = gdjs.paquimetroCode.GDnanioObjects1[i];
-        ++k;
-    }
-}
-gdjs.paquimetroCode.GDnanioObjects1.length = k;}if ( gdjs.paquimetroCode.condition0IsTrue_0.val ) {
-{
-gdjs.paquimetroCode.condition1IsTrue_0.val = gdjs.evtTools.object.hitBoxesCollisionTest(gdjs.paquimetroCode.mapOfGDgdjs_46paquimetroCode_46GDblockNanioPonto0Objects1Objects, gdjs.paquimetroCode.mapOfGDgdjs_46paquimetroCode_46GDnanioObjects1Objects, false, runtimeScene, false);
-}}
-if (gdjs.paquimetroCode.condition1IsTrue_0.val) {
-/* Reuse gdjs.paquimetroCode.GDnanioObjects1 */
-{for(var i = 0, len = gdjs.paquimetroCode.GDnanioObjects1.length ;i < len;++i) {
-    gdjs.paquimetroCode.GDnanioObjects1[i].clearForces();
-}
-}}
-
-}
-
-
-{
-
-gdjs.copyArray(runtimeScene.getObjects("blockNanioPonto0"), gdjs.paquimetroCode.GDblockNanioPonto0Objects1);
-gdjs.copyArray(runtimeScene.getObjects("blockNanioPonto90"), gdjs.paquimetroCode.GDblockNanioPonto90Objects1);
+gdjs.paquimetroCode.condition0IsTrue_0.val = gdjs.evtTools.input.getMouseX(runtimeScene, "", 0) <= 50;
+}if (gdjs.paquimetroCode.condition0IsTrue_0.val) {
 gdjs.copyArray(runtimeScene.getObjects("nanio"), gdjs.paquimetroCode.GDnanioObjects1);
+{for(var i = 0, len = gdjs.paquimetroCode.GDnanioObjects1.length ;i < len;++i) {
+    gdjs.paquimetroCode.GDnanioObjects1[i].activateBehavior("Draggable", false);
+}
+}}
+
+}
+
+
+{
+
 
 gdjs.paquimetroCode.condition0IsTrue_0.val = false;
-gdjs.paquimetroCode.condition1IsTrue_0.val = false;
 {
-for(var i = 0, k = 0, l = gdjs.paquimetroCode.GDnanioObjects1.length;i<l;++i) {
-    if ( gdjs.paquimetroCode.GDnanioObjects1[i].getBehavior("Draggable").isDragged() ) {
-        gdjs.paquimetroCode.condition0IsTrue_0.val = true;
-        gdjs.paquimetroCode.GDnanioObjects1[k] = gdjs.paquimetroCode.GDnanioObjects1[i];
-        ++k;
-    }
-}
-gdjs.paquimetroCode.GDnanioObjects1.length = k;}if ( gdjs.paquimetroCode.condition0IsTrue_0.val ) {
-{
-gdjs.paquimetroCode.condition1IsTrue_0.val = gdjs.evtTools.object.hitBoxesCollisionTest(gdjs.paquimetroCode.mapOfGDgdjs_46paquimetroCode_46GDblockNanioPonto0Objects1Objects, gdjs.paquimetroCode.mapOfGDgdjs_46paquimetroCode_46GDblockNanioPonto90Objects1Objects, false, runtimeScene, false);
-}}
-if (gdjs.paquimetroCode.condition1IsTrue_0.val) {
-/* Reuse gdjs.paquimetroCode.GDnanioObjects1 */
+gdjs.paquimetroCode.condition0IsTrue_0.val = !(gdjs.evtTools.input.getMouseX(runtimeScene, "", 0) <= 50);
+}if (gdjs.paquimetroCode.condition0IsTrue_0.val) {
+gdjs.copyArray(runtimeScene.getObjects("nanio"), gdjs.paquimetroCode.GDnanioObjects1);
 {for(var i = 0, len = gdjs.paquimetroCode.GDnanioObjects1.length ;i < len;++i) {
-    gdjs.paquimetroCode.GDnanioObjects1[i].clearForces();
+    gdjs.paquimetroCode.GDnanioObjects1[i].activateBehavior("Draggable", true);
+}
+}}
+
+}
+
+
+{
+
+
+gdjs.paquimetroCode.condition0IsTrue_0.val = false;
+{
+gdjs.paquimetroCode.condition0IsTrue_0.val = gdjs.evtTools.input.getMouseX(runtimeScene, "", 0) >= 1400;
+}if (gdjs.paquimetroCode.condition0IsTrue_0.val) {
+gdjs.copyArray(runtimeScene.getObjects("nanio"), gdjs.paquimetroCode.GDnanioObjects1);
+{for(var i = 0, len = gdjs.paquimetroCode.GDnanioObjects1.length ;i < len;++i) {
+    gdjs.paquimetroCode.GDnanioObjects1[i].activateBehavior("Draggable", false);
+}
+}}
+
+}
+
+
+{
+
+
+gdjs.paquimetroCode.condition0IsTrue_0.val = false;
+{
+gdjs.paquimetroCode.condition0IsTrue_0.val = !(gdjs.evtTools.input.getMouseX(runtimeScene, "", 0) >= 1400);
+}if (gdjs.paquimetroCode.condition0IsTrue_0.val) {
+gdjs.copyArray(runtimeScene.getObjects("nanio"), gdjs.paquimetroCode.GDnanioObjects1);
+{for(var i = 0, len = gdjs.paquimetroCode.GDnanioObjects1.length ;i < len;++i) {
+    gdjs.paquimetroCode.GDnanioObjects1[i].activateBehavior("Draggable", true);
 }
 }}
 
